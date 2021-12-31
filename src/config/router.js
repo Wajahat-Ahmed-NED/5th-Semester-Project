0bef61521d0af80ec982fdf225d844ee6baa2e37
Abdul Moiz <moizrauf03@gmail.com>
2022-01-01 00:08:08 +0500

    NavBar Commit. Moiz

--- src/config/router.js
+++ src/config/router.js
@@ -1,14 +1,12 @@
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
+import { BrowserRouter as Router,  Routes,  Route,} from "react-router-dom";
 
-} from "react-router-dom";
+
 import FloatingActionButtons from "../components/MuiButton";
 import Dashboard from "../screen/Dashboard";
 import Login from "../screen/Login";
 import Signup from "../screen/Signup";
+import Navbar2 from "../components/Navbar2";
 
 export default function AppRouter() {
   return (
@@ -18,6 +16,7 @@ export default function AppRouter() {
           <Route path='/' element={<Dashboard/>}/>
           <Route path='/Signup' element={<Signup/>}/>
           <Route path='/Login' element={<Login/>}/>
+          <Route path='/Navbar2' element={<Navbar2/>}/>
           <Route path='/muibutton' element={<FloatingActionButtons/>}/>
         </Routes>
       </div>
